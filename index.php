1f6723a3e0102efb14cd594c8e3112360feaad6b
UtmostSkylark <romazahrypa@gmail.com>
2019-02-05 15:11:58 +0200

    Model with Db connection using MYSQLI designed

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ if(isset($_GET['option'])){
             break;
         default:
             $init = new Index;
+        break;
     }
 }else{
     $init = new Index;
---
+++ models/Model.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Skylark
+ * Date: 2/5/2019
+ * Time: 2:49 PM
+ */
+
+class Model
+{
+    public $db;
+
+    public function __construct($host, $user, $pass, $db)
+    {
+        $this->db = mysqli_connect($host, $user, $pass, $db);
+        if($this->db){
+            exit("No connection with DB");
+        }
+        if(!mysqli_select_db($this->db, $db)){
+            exit('Your DB is empty');
+        }
+
+        mysqli_set_charset($this->db, 'utf8'); //mysqli_query($this->db, "SET NAMES utf-8"); -- not recommended. but can be used
+
+        return $this->db;
+
+    }
+
+    public function getAllPosts(){
+        $sql = "SELECT * FROM post LIMIT 10";
+        $res = mysqli_query($this->db, $sql);
+        $rows = null;
+
+        if(!$res){
+            return false;
+        }else{
+            for ($i = 0; $i < mysqli_num_rows($res); $i++){
+                $rows [] = mysqli_fetch_array($res, MYSQLI_ASSOC);
+            }
+        }
+
+        return $rows;
+    }
+
+    public function getSinglePost($id){
+        $sql = "SELECT * FROM post WHERE id={(int)$id} LIMIT 1";
+        $res = mysqli_query($this->db, $sql);
+        if(!$res){
+            return false;
+        }
+        $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+        return $row;
+    }
+}
